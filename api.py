5f7ef4de195c72bfd735c6b9d62bb885638985f3
Григорий Кузнецов <kuznetsov.grigoy@outlook.com>
2023-04-26 23:29:09 +0300

    функционал запроса API с работы

---
+++ api.py
@@ -0,0 +1,42 @@
+import os
+from datetime import date
+from http import HTTPStatus
+
+import requests
+from dotenv import load_dotenv
+
+from models import Train
+
+load_dotenv()
+
+YANDEX_SCHEDULE_KEY = os.getenv('YANDEX_SCHEDULE_KEY')
+API_URL = 'https://api.rasp.yandex.net/v3.0/search/?'
+
+params = {
+    'apikey': YANDEX_SCHEDULE_KEY,
+    'from':'s9600741',
+    'to':'s9600901',
+    'date': date.today(),
+    'limit':5
+}
+
+def get_trains_info():
+    respose = requests.get(API_URL, params=params)
+
+    if respose.status_code == HTTPStatus.OK:
+        results = respose.json()['segments']
+        trains = []
+        for result in results:
+            train = Train(
+                train_type=result['thread']['transport_subtype']['title'],
+                from_station=result['from']['title'],
+                to_station=result['to']['title'],
+                departure=result['departure'],
+                arrival=result['arrival']
+            )
+            trains.append(train)
+        return trains
+
+
+
+
