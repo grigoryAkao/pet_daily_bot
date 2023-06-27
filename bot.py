54a9fec13ee75fd2822e1b0e5bb64695b547c349
Григорий Кузнецов <kuznetsov.grigoy@outlook.com>
2023-06-27 22:55:43 +0300

    функционально рабочая версия

--- bot.py
+++ bot.py
@@ -3,8 +3,8 @@ import os
 from dotenv import load_dotenv
 
 import logging
-from telegram import Update
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, CallbackQueryHandler
 
 from api import get_trains_info
 
@@ -19,10 +19,24 @@ logging.basicConfig(
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(
-        chat_id=update.effective_chat.id,
-        text='Бот работает и готов помочь добраться домой.'
-    )
+    keyboard = [
+        [
+            InlineKeyboardButton("Get trains info", callback_data="get_trains"),
+        ],
+    ]
+
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    await update.message.reply_text("Please choose:", reply_markup=reply_markup)
+
+async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+
+    """Parses the CallbackQuery and updates the message text."""
+
+    query = update.callback_query
+
+    await query.answer()
+    await query.edit_message_text(text=f"Selected option: {query.data}")
 
 async def get_trains(update: Update, context: ContextTypes.DEFAULT_TYPE):
     trains = get_trains_info()
@@ -37,8 +51,9 @@ if __name__ == '__main__':
     application = ApplicationBuilder().token(TELEGRAM_BOT_TOKEN).build()
 
     start_handler = CommandHandler('start', start)
-    train_handler = CommandHandler('get_trains', get_trains)
+    get_trains = CallbackQueryHandler(button)
     application.add_handler(start_handler)
-    application.add_handler(train_handler)
+    application.add_handler(get_trains)
+
 
     application.run_polling()
